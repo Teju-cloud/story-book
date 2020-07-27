f538594ae2847f1e4b21b2c47ef518bbbc958618
theju <theju0190@gmail.com>
2020-07-27 08:33:11 +0530

    Task: Login page using story book

---
+++ src/components/Login/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+import './login.css'
+
+function Login() {
+
+    const [login, setLogin] = useState({
+        usn: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        console.log(e.target)
+        setLogin(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log(login)
+    }
+
+    return (
+        <div className="login-container">
+            <form>
+                <h1>Login</h1>
+                <div className="input-feilds">
+                    <label>USN</label>
+                    <input 
+                    type="text" 
+                    id='usn'
+                    value={login.usn}
+                    onChange={handleChange}
+                    placeholder="Your University Registration Number"/>
+                </div>
+              
+                <div className="input-feilds">
+                    <label>Password</label>
+                    <input 
+                    type="password" 
+                    id="password"
+                    value={login.password}
+                    onChange={handleChange}
+                    placeholder="Password"/>
+                </div>
+               
+                <button onClick={handleLogin}>Login</button>
+
+                <a href="#">Forgot Password? </a>
+            </form>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/stories/2-Login.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Login from '../components/Login'
+
+export default {
+    title: 'Login',
+    component: Login ,
+  };
+
+export const ToLogin = () => <Login/>
